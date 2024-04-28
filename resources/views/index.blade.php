f8fa7a7cd1a9e3a088284fcdd92d4c89196b45fb
f4uz1rm <f4uz1rm@gmail.com>
2024-04-29 05:45:36 +0700

    Update service item numbers in index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -245,7 +245,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="services-widget position-relative">
-                            <div class="service-item current d-flex flex-wrap align-items-center wow fadeInUp"
+                            {{-- <div class="service-item current d-flex flex-wrap align-items-center wow fadeInUp"
                                 data-wow-delay=".5s">
                                 <div class="left-box d-flex flex-wrap align-items-center">
                                     <span class="number">01</span>
@@ -258,11 +258,11 @@
                                 </div>
                                 <i class="flaticon-up-right-arrow"></i>
                                 <button data-mfp-src="#service-wrapper" class="service-link modal-popup"></button>
-                            </div>
+                            </div> --}}
                             <div class="service-item d-flex flex-wrap align-items-center wow fadeInUp"
                                 data-wow-delay=".6s">
                                 <div class="left-box d-flex flex-wrap align-items-center">
-                                    <span class="number">02</span>
+                                    <span class="number">01</span>
                                     <h3 class="service-title">Web Design</h3>
                                 </div>
                                 <div class="right-box">
@@ -276,7 +276,7 @@
                             <div class="service-item d-flex flex-wrap align-items-center wow fadeInUp"
                                 data-wow-delay=".7s">
                                 <div class="left-box d-flex flex-wrap align-items-center">
-                                    <span class="number">03</span>
+                                    <span class="number">02</span>
                                     <h3 class="service-title">UI Design</h3>
                                 </div>
                                 <div class="right-box">
@@ -290,7 +290,7 @@
                             <div class="service-item d-flex flex-wrap align-items-center wow fadeInUp"
                                 data-wow-delay=".8s">
                                 <div class="left-box d-flex flex-wrap align-items-center">
-                                    <span class="number">04</span>
+                                    <span class="number">03</span>
                                     <h3 class="service-title">Graphics Design</h3>
                                 </div>
                                 <div class="right-box">
